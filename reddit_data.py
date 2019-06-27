12c35335a9c98c52d32496ebaf3efa65c337f44d
Ziyad Dadabhoy <ziyad.dadabhoy@gmail.com>
2019-06-27 11:01:17 -0500

    Update reddit_data.py

--- reddit_data.py
+++ reddit_data.py
@@ -9,21 +9,18 @@ def bot_login():
                          password=config.reddit_password,
                          client_id=config.client_id,
                          client_secret=config.client_secret,
-                         user_agent="HWSearch App v1.0")
+                         user_agent="Manga Disc Site V.1")
     return reddit
 
 
 def get_from_reddit(item):
     results = {}
     reddit = bot_login()
-    reddit_posts = reddit.subreddit('hardwareswap').search(
+    reddit_posts = reddit.subreddit('manga').search(
         item,
         sort='new',
         time_filter='week')
     for item in reddit_posts:
-        if item.link_flair_text is 'Closed' or (item.title.startswith('[CAN]') or item.title.startswith('[GBR]')):
-            continue
-        location = re.findall('USA\s?\-?\s?\w+', item.title)
         results[item.title] = {'location': location,
                                'type': item.link_flair_text,
                                'text': item.selftext,
